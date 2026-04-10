935f43ead0263793a863be3381d0b1475da21168
Edouard Demotes <e.demotes@gmail.com>
2026-04-10 15:32:43 -0400

    feat: add confirmation message display in settings tab after saving changes

--- src/live_stt/gui/settings_tab.py
+++ src/live_stt/gui/settings_tab.py
@@ -10,6 +10,8 @@ from ..services import logger
 
 log = logger.get(__name__)
 
+_SAVE_MESSAGE = "Settings saved. You may need to restart the application for all changes to take effect."
+
 _GENERAL_SPEC = [
     ("hotkey", "Hotkey", "entry"),
     ("model_name", "Model", "entry"),
@@ -74,7 +76,6 @@ def _build_section(title: str, specs: list, config: Config, entries: dict) -> Gt
     frame.add(grid)
     return frame
 
-
 class SettingsTab(Gtk.ScrolledWindow):
     def __init__(self, config: Config, on_save):
         super().__init__()
@@ -103,6 +104,10 @@ class SettingsTab(Gtk.ScrolledWindow):
         save_btn.connect("clicked", self._on_save_clicked)
         box.pack_start(save_btn, False, False, 0)
 
+        self._info_label = Gtk.Label()
+        self._info_label.set_no_show_all(True)
+        box.pack_start(self._info_label, False, False, 0)
+
         self.add(box)
 
     def _on_save_clicked(self, _btn: Gtk.Button) -> None:
@@ -124,3 +129,5 @@ class SettingsTab(Gtk.ScrolledWindow):
 
         self._cfg.save()
         self._on_save()
+        self._info_label.set_text(_SAVE_MESSAGE)
+        self._info_label.show()
